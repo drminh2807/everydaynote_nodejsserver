0f97c93a30e5ce661729b17c88cab78424e55d35
Minh DV <drminh2807@gmail.com>
2017-05-03 15:53:56 +0700

    Demo

--- index.js
+++ index.js
@@ -1,42 +1,66 @@
-var express = require("express");
-var mysql = require('mysql');
+// var express = require("express");
+// var mysql = require('mysql');
+// var app = express();
+
+// var pool = mysql.createPool({
+//     connectionLimit: 100, //important
+//     host: 'sql12.freemysqlhosting.net',
+//     user: 'sql12172476',
+//     password: 'r7N39vKaSU',
+//     database: 'sql12172476',
+//     debug: false
+// });
+
+// function handle_database(req, res) {
+
+//     pool.getConnection(function (err, connection) {
+//         if (err) {
+//             res.json({ "code": 100, "status": "Error in connection database" });
+//             return;
+//         }
+
+//         console.log('connected as id ' + connection.threadId);
+
+//         connection.query("select * from user", function (err, rows) {
+//             connection.release();
+//             if (!err) {
+//                 res.json(rows);
+//             }
+//         });
+
+//         connection.on('error', function (err) {
+//             res.json({ "code": 100, "status": "Error in connection database" });
+//             return;
+//         });
+//     });
+// }
+
+// app.get("/", function (req, res) {
+//     handle_database(req, res);
+// });
+
+// app.listen(3000);
+
+var cool = require('cool-ascii-faces');
+var express = require('express');
 var app = express();
 
-var pool = mysql.createPool({
-    connectionLimit: 100, //important
-    host: 'sql12.freemysqlhosting.net',
-    user: 'sql12172476',
-    password: 'r7N39vKaSU',
-    database: 'sql12172476',
-    debug: false
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('pages/index')
 });
 
-function handle_database(req, res) {
-
-    pool.getConnection(function (err, connection) {
-        if (err) {
-            res.json({ "code": 100, "status": "Error in connection database" });
-            return;
-        }
-
-        console.log('connected as id ' + connection.threadId);
-
-        connection.query("select * from user", function (err, rows) {
-            connection.release();
-            if (!err) {
-                res.json(rows);
-            }
-        });
-
-        connection.on('error', function (err) {
-            res.json({ "code": 100, "status": "Error in connection database" });
-            return;
-        });
-    });
-}
-
-app.get("/", function (req, res) {
-    handle_database(req, res);
+app.get('/cool', function(request, response) {
+  response.send(cool());
 });
 
-app.listen(3000);
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
